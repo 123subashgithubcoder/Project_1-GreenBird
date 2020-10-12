2eae9d3a7e805cc676bee86656d5fcbf4f64b7e7
SUBZzz <45166753+123subashgithubcoder@users.noreply.github.com>
2020-10-12 20:09:19 +0530

    UNITY SETUP
    Creating files for setting up the project

---
+++ GreenBird/Assets/Bird.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private void onMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+}
